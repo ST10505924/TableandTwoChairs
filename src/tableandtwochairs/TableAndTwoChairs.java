59b347dfd6f1f9d720ad08d9dda0487095f24c31
ST10505924 <ST10505924@myemeris.edu.za>
2026-03-01 20:55:30 +0200

    First Commit

---
+++ src/tableandtwochairs/TableAndTwoChairs.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tableandtwochairs;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class TableAndTwoChairs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("x  \t\t\t\t    x");
+        System.out.println("x  \t\t\t\t    x");
+        System.out.println("x  \t\t\t\t    x");
+        System.out.println("x  \t    xxxxxxxxxxxxx\t    x");
+        System.out.println("xxxxxxx     x\t\tx     xxxxxxx");
+        System.out.println("x     x     x\tTABLE\tx     x\t    x");
+        System.out.println("x     x     x\t\tx     x\t    x");
+        System.out.println("x     x     x\t\tx     x\t    x");
+    }   
+    
+}
